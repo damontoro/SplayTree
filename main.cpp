399080729b9f45dd6a986de238b079cf5c39d9a2
Souls <dmonto01@ucm.es>
2022-12-23 19:06:05 +0100

    Created a main for future testing

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Splaytree.cpp"
+
+using namespace std;
+
+auto main(void) -> int {
+
+	Splaytree<int> tree;
+	tree.insert(5);
+	tree.insert(3);
+	tree.insert(7);
+
+}
